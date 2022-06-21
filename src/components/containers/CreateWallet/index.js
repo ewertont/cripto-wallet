91580aad114dd0178aa16eb847e738ec49bdbcb4
Ewerton Tavares <ewerton_tavares@live.com>
2022-06-21 15:33:56 -0300

    update aula 2

--- src/components/containers/CreateWallet/index.js
+++ src/components/containers/CreateWallet/index.js
@@ -1,19 +1,28 @@
-import React from 'react'
-import { CreateWalletContainer } from './styled'
+import React from 'react';
+import Box from '../../Box';
+import {
+  CreateWalletContainer,
+  CreateWalletTittle,
+  CreateWalletKey,
+  LinkToMyWallet,
+} from './styled';
 
 function CreateWallet() {
   return (
     <CreateWalletContainer>
       <header>
-        <h2 className='light'>Please save your private key in a secure place</h2>
+        <CreateWalletTittle className="light">
+          Please save your private key in a secure place
+        </CreateWalletTittle>
       </header>
-      <article>
-        <p className='primary'>fa1230821930812309182903812093812039182309821093812039821093821093</p>
-      </article>
-      <a href='#'>To my wallet</a>  
+      <Box>
+        <CreateWalletKey className="primary">
+          fa1230821930812309182903812093812039182309821093812039821093821093
+        </CreateWalletKey>
+      </Box>
+      <LinkToMyWallet to="/wallet">To my Wallet</LinkToMyWallet>
     </CreateWalletContainer>
-
-  )
+  );
 }
 
-export default CreateWallet
+export default CreateWallet;
--- src/components/containers/CreateWallet/styled.js
+++ src/components/containers/CreateWallet/styled.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import Box from '../../Box';
+import { Link } from 'react-router-dom';
 
 export const CreateWalletContainer = styled.section`
   width: 100%;
@@ -9,9 +11,27 @@ export const CreateWalletContainer = styled.section`
   align-items: center;
   justify-content: center;
   flex-direction: column;
+  ${Box} {
+    padding: 103px 53px;
+    width: 100%;
+    text-align: center;
+    margin-bottom: 69px;
+  }
+`;
+
+export const LinkToMyWallet = styled(Link)`
+  text-decoration: none;
+  color: var(--primaryColor);
 `;
 
 export const CreateWalletTittle = styled.p`
   font-size: 26px;
-  
+`;
+
+export const CreateWalletKey = styled.p`
+  font-size: 32px;
+  word-break: break-all;
+  margin: 0;
+  font-weight: 400;
+  font-style: italic;
 `;
